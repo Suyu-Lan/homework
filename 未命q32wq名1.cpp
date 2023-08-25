411520fc2110f5a9025b412c44fb2445c800e2de
Suyu-Lan <143158820+Suyu-Lan@users.noreply.github.com>
2023-08-25 14:03:49 +0800

    Add files via upload

---
+++ 未命q32wq名1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long a[1005];
+long long w(int n){
+	cout<<n<<endl;
+	if(n==2||n==1)return 1;
+	if(a[n]==0)a[n]=w(n-1)+w(n-2);
+	return a[n]; 
+}
+int main(){
+	int n=0;
+	cin>>n;
+
+	cout<<endl<<w(n)<<endl;
+	for(int i=0;i<n;i++){
+		cout<<a[i]<<" ";
+	}	
+} 
